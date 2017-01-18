edaa30e2bc5c37d7f521ca953d765f93094f3c07
Mikhail Semin <bifot@bifot.ru>
2017-01-18 21:02:28 +0300

    Добавил файлы.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # VKPostsGrabber
+
 Ретрансляция постов из ВКонтакте в Telegram. Теперь заходить в ВК вовсе не обязательно, все в любимом телеграме!
+
+## Установка
+
+```
+$ git clone https://github.com/bifot/VKPostsGrabber
+$ cd VKPostsGrabber
+$ npm install
+$ node app.js
+```
+_________
+  
+*Сделано с любовью, автор: [Михаил Семин](http://bifot.ru).*
---
+++ app.js
@@ -0,0 +1,20 @@
+const TelegramBot = require('node-telegram-bot-api');
+const request = require('sync-request');
+const config = require('./config');
+
+const token = config.token;
+
+const bot = new TelegramBot(token, { polling: true });
+
+const start = require('./modules/cmd/start')(bot);
+const parser = require('./modules/cmd/parser')(bot, request);
+
+bot.on('message', msg => {
+  console.log(`Пользователь @${msg.from.username} написал «${msg.text}»`);
+
+  if (msg.text == '/start') {
+    start.cmd(msg);
+  } else {
+    parser.cmd(msg);
+  }
+});
