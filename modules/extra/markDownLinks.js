1cb3533149fab3141d04eb2da00c704d0215178e
Mikhail Semin <bifot@bifot.ru>
2017-02-02 10:47:40 +0300

    :bug: Поправил вики-разметку.

--- modules/extra/markDownLinks.js
+++ modules/extra/markDownLinks.js
@@ -1,18 +1,16 @@
 var markdownLinks = () => {
-  return () => {
-    // Находим ссылки с ВК-разметкой
-    var markdownLinks = _message.match(/\[([clubid]){0,}([0-9]){0,}\|.([\w\sа-яА-ЯёЁ.]){0,}.\S{0,}\]/g);
+  // Находим ссылки с ВК-разметкой
+  var markdownLinks = _message.match(/\[([clubid]){0,}([0-9]){0,}\|.([\w\sа-яА-ЯёЁ.]){0,}.\S{0,}\]/g);
 
-    // Заменяем ссылки в ВК-разметке
-    if (markdownLinks) {
-      markdownLinks.forEach(link => {
-        var url = link.split('|')[0].substr(1);
-        var title = link.split('|')[1].slice(0, -1);
-        var linkToPage = `<a href="https://vk.com/${url}">${title}</a>`;
+  // Заменяем ссылки в ВК-разметке
+  if (markdownLinks) {
+    markdownLinks.forEach(link => {
+      var url = link.split('|')[0].substr(1);
+      var title = link.split('|')[1].slice(0, -1);
+      var linkToPage = `<a href="https://vk.com/${url}">${title}</a>`;
 
-        _message = _message.replace(link, linkToPage);
-      });
-    }
+      _message = _message.replace(link, linkToPage);
+    });
   }
 };
 
